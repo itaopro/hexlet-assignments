c122dacf099564df778d48c79bcbd72d90e5e563
@hexlet/cli <support@hexlet.io>
2023-10-20 15:33:34 +0300

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -0,0 +1,49 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import io.javalin.http.NotFoundResponse;
+import exercise.model.User;
+import exercise.dto.users.UserPage;
+import exercise.dto.users.UsersPage;
+import java.util.Collections;
+
+public final class App {
+
+    // Каждый пользователь представлен объектом класса User
+    private static final List<User> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+        app.get("/users", ctx -> {
+            ctx.json(USERS);
+        });
+
+        app.get("/users/{id}", ctx -> {
+            long id = Long.parseLong(ctx.pathParam("id"));
+            User user = USERS.stream()
+                    .filter(u -> u.getId() == id)
+                    .findFirst()
+                    .orElseThrow(() -> new NotFoundResponse("User not found"));
+            ctx.json(user);
+        });
+
+        // END
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
